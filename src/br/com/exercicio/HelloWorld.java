2e6fd0421065579fdbdc7c53998488e535f4eed5
wbarcellosn <wbarcellosn@gmail.com>
2024-04-13 11:05:37 -0300

    commit inicial

---
+++ src/br/com/exercicio/HelloWorld.java
@@ -0,0 +1,9 @@
+package br.com.exercicio;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.print("Este é o 'Hello World!' feito para o exercício do módulo 4");
+
+    }
+}
